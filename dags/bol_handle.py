6f99ca153273c49862aadd3a496a2e40f188ce96
trdtnguyen <trauconnguyen@gmail.com>
2020-11-29 13:50:56 -0600

    finish dags for stock and bol

---
+++ dags/bol_handle.py
@@ -0,0 +1,49 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import timedelta, datetime
+from tasks.BOL import BOL
+
+WORKFLOW_DAG_ID = 'bol_handle'
+WORKFLOW_START_DATE = datetime.now() - timedelta(days=1)
+
+# using CRON format
+WORKFLOW_SCHEDULE_INTERVAL = None
+
+WORKFLOW_DEFAULT_ARGS = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": WORKFLOW_START_DATE,
+    "email": ["sb@opened_capstone.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+}
+
+dag = DAG(
+    WORKFLOW_DAG_ID,
+    description="ETL for BOL data",
+    schedule_interval=WORKFLOW_SCHEDULE_INTERVAL,
+    default_args=WORKFLOW_DEFAULT_ARGS,
+    catchup=False,
+)
+
+bol = BOL()
+start_year = 2010
+end_year = datetime.now().year
+
+t1 = PythonOperator(
+        task_id='extract_BOL',
+        python_callable=bol.extract_BOL,
+        op_kwargs={'start_year': start_year,
+                   'end_year': end_year
+                  },
+        dag=dag,
+)
+
+t2 = PythonOperator(
+        task_id='transform_raw_to_fact_bol',
+        python_callable=bol.transform_raw_to_fact_bol,
+        dag=dag,
+)
+
+t1 >> t2
---
+++ dags/stock_handle.py
@@ -0,0 +1,61 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import timedelta, datetime
+from tasks.Stock import Stock
+
+WORKFLOW_DAG_ID = 'stock_handle'
+WORKFLOW_START_DATE = datetime.now() - timedelta(days=1)
+
+#using CRON format
+WORKFLOW_SCHEDULE_INTERVAL = None
+
+WORKFLOW_DEFAULT_ARGS = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": WORKFLOW_START_DATE,
+    "email": ["sb@opened_capstone.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+}
+
+dag = DAG(
+    WORKFLOW_DAG_ID,
+    description="ETL for stock data",
+    schedule_interval=WORKFLOW_SCHEDULE_INTERVAL,
+    default_args=WORKFLOW_DEFAULT_ARGS,
+    catchup=False,
+)
+
+stock = Stock()
+
+t1 = PythonOperator(
+        task_id='extract_sp500',
+        python_callable=stock.extract_sp500_tickers,
+        dag=dag,
+)
+
+start_date = datetime(2020, 1, 1)
+end_date = datetime.now()
+t2 = PythonOperator(
+        task_id='extract_batch_stock',
+        python_callable=stock.extract_batch_stock,
+        op_kwargs={'reload': True,
+               'ticker_file': Stock.DEFAULT_TICKER_FILE,
+               'start_date': start_date,
+               'end_date': end_date},
+        dag=dag,
+)
+
+t3 = PythonOperator(
+        task_id='transform_raw_to_fact_stock',
+        python_callable=stock.transform_raw_to_fact_stock,
+        dag=dag,
+)
+t4 = PythonOperator(
+        task_id='aggregate_fact_to_monthly_fact_stock',
+        python_callable=stock.aggregate_fact_to_monthly_fact_stock,
+        dag=dag,
+)
+
+t1 >> t2 >> t3 >> t4
