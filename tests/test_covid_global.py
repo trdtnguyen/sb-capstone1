a7d0c8cb1db12e8014d66e781a43cbef3932c044
trdtnguyen <trauconnguyen@gmail.com>
2021-01-19 09:39:15 -0600

    finish test cases for covid_global

--- tests/test_covid_global.py
+++ tests/test_covid_global.py
@@ -29,3 +29,42 @@ def test_transform_raw_to_dim_country():
     # Read after write
     dim_df = GU.read_from_db(spark, DIM_TABLE_NAME)
     assert dim_df.count() > 0
+
+
+def test_extract_global():
+    covid.extract_global()
+
+    latest_df, is_resume_extract, latest_date = GU.read_latest_data(spark, RAW_TABLE_NAME)
+    assert (is_resume_extract is not None)
+    assert is_resume_extract
+    assert (latest_date > GU.START_DEFAULT_DATE)
+
+    # Read after write
+    raw_df = GU.read_from_db(spark, RAW_TABLE_NAME)
+    assert raw_df.count() > 0
+
+
+def test_transform_raw_to_fact_global():
+    covid.transform_raw_to_fact_global()
+
+    latest_df, is_resume_extract, latest_date = GU.read_latest_data(spark, FACT_TABLE_NAME)
+    assert (is_resume_extract is not None)
+    assert is_resume_extract
+    assert (latest_date > GU.START_DEFAULT_DATE)
+
+    # Read after write
+    df = GU.read_from_db(spark, FACT_TABLE_NAME)
+    assert df.count() > 0
+
+
+def test_aggregate_fact_to_monthly_fact_global():
+    covid.aggregate_fact_to_monthly_fact_global()
+
+    latest_df, is_resume_extract, latest_date = GU.read_latest_data(spark, MONTHLY_FACT_TABLE_NAME)
+    assert (is_resume_extract is not None)
+    assert is_resume_extract
+    assert (latest_date > GU.START_DEFAULT_DATE)
+
+    # Read after write
+    df = GU.read_from_db(spark, MONTHLY_FACT_TABLE_NAME)
+    assert df.count() > 0
